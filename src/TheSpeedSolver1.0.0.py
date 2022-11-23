eeb8c2d7b7fd664ce8de1968d57838e95ae579a3
Kipp Puffalt <puffaltk@gmail.com>
2022-11-22 21:02:22 -0700

    Started to make a way of keeping track of a cube.

---
+++ src/TheSpeedSolver1.0.0.py
@@ -0,0 +1,54 @@
+class Cube:
+    cubies = [
+        ['W','R','B']
+        ['W','B','O']
+        ['W','O','G']
+        ['W','G','R']
+
+        ['R','B','Y']
+        ['B','O','Y']
+        ['O','G','Y']
+        ['G','R','Y']
+    ]
+
+    def u():
+        # todo 
+    
+    def up():
+        #todo
+    def ut():
+        #todo
+    
+    def d():
+        #todo
+    def dp():
+        #todo
+    def dt():
+        #todo
+    
+    def r():
+        #todo
+    def rp():
+        #todo
+    def rt():
+    
+    def l():
+        #todo
+    def lp():
+        #todo
+    def lt():
+        #todo
+    
+    def f():
+        #todo
+    def fp():
+        #todo
+    def ft():
+        #todo
+    
+    def b():
+        #todo
+    def bp():
+        #todo
+    def bt():
+        #todo
