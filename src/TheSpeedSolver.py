ca386d037ba97ccd13f119595d920c625234e83e
Kipp Puffalt <puffaltk@gmail.com>
2022-11-28 20:53:53 -0700

    Added some 180 turn functions

--- src/TheSpeedSolver.py
+++ src/TheSpeedSolver.py
@@ -12,40 +12,49 @@ class Cube:
             ['G','R','Y']
         ]
 
-    # does a clockwise twist to top of the cube the cube
+    # does a clockwise twist to top of the cube
     def u(self):
         self.cubies[0], self.cubies[2], self.cubies[2], self.cubies[3] = self.cubies[3], self.cubies[0], self.cubies[1], self.cubies[2]
+    # does a counterclockwise twist to top of the cube
     def up(self):
         self.cubies[0], self.cubies[1], self.cubies[2], self.cubies[3] = self.cubies[3], self.cubies[0], self.cubies[1], self.cubies[2]
-    # def ut(self):
-        #todo
-
+    # does a 180 turn to the top of the cube
+    def ut(self):
+        self.cubies[0], self.cubies[4], self.cubies[7], self.cubies[3] = self.cubies[7], self.cubies[3], self.cubies[0], self.cubies[4]
+    
     # does a clockwise turn to the bottom layer
     def d(self):
         self.cubies[4], self.cubies[5], self.cubies[6], self.cubies[7] = self.cubies[7], self.cubies[4], self.cubies[5], self.cubies[6]
+    # does a counterclockwise turn to the bottom layer
     def dp(self):
         self.cubies[4], self.cubies[5], self.cubies[6], self.cubies[7] = self.cubies[5], self.cubies[6], self.cubies[7], self.cubies[4]
-    # def dt(self):
-        #todo
+    # does a 180 turn  to the bottom layer
+    def dt(self):
+        self.cubies[4], self.cubies[5], self.cubies[6], self.cubies[7] = self.cubies[6], self.cubies[7], self.cubies[4], self.cubies[5]
 
     # does a turn to the right hand side clockwise
     def r(self):
         self.cubies[1], self.cubies[5], self.cubies[6], self.cubies[2] = self.cubies[5], self.cubies[6], self.cubies[2], self.cubies[1]
+    # does a turn to the right hand side counterclockwise
     def rp(self):
-        self.cubies[1], self.cubies[5], self.cubies[6], self.cubies[2] = self.cubies[2], self.cubies[1], self.cubies[5], self.cubies[6]
-    # def rt(self):
+        self.cubies[1], self.cubies[5], self.cubies[6], self.cubies[2] = self.cubies[2], self.cubies[1], self.cubies[5], self.cubies[6]    
+    # does a 180 turn to the right han side of the cube
+    def rt(self):
+        self.cubies[1], self.cubies[5], self.cubies[6], self.cubies[2] = self.cubies[6], self.cubies[2], self.cubies[1], self.cubies[5]    
     
     # does a turn to the left hand side clockwise
     def l(self):
         self.cubies[0], self.cubies[4], self.cubies[7], self.cubies[3] = self.cubies[3], self.cubies[0], self.cubies[4], self.cubies[7]
+    # does a turn to the left hand side counterclockwise
     def lp(self):
         self.cubies[0], self.cubies[4], self.cubies[7], self.cubies[3] = self.cubies[4], self.cubies[7], self.cubies[3], self.cubies[0]
-    # def lt(self8):
+    # def lt(self):
         #todo
     
     # does a clockwise turn to the front of the cube
     def f(self):
         self.cubies[0], self.cubies[4], self.cubies[7], self.cubies[3] = self.cubies[3], self.cubies[0], self.cubies[4], self.cubies[7]
+    # does a counterclockwise turn to the front of the cube
     def fp(self):
         self.cubies[0], self.cubies[4], self.cubies[7], self.cubies[3] = self.cubies[4], self.cubies[7], self.cubies[3], self.cubies[0]
     # def ft(self):
@@ -54,6 +63,7 @@ class Cube:
     # does a clockwise turn to the backside of the cube
     def b(self):
         self.cubies[3], self.cubies[7], self.cubies[6], self.cubies[2] = self.cubies[2], self.cubies[3], self.cubies[7], self.cubies[6]
+    # does a counterclockwise turn to the backside of the cube
     def bp(self):
         self.cubies[2], self.cubies[6], self.cubies[7], self.cubies[3] = self.cubies[3], self.cubies[2], self.cubies[6], self.cubies[7]
     # def bt(self):
@@ -66,6 +76,6 @@ cube = Cube()
 print('test: ', end = '')
 cube.render()
 
-cube.bp()
+cube.rt()
 print('test: ', end = '')
 cube.render()
