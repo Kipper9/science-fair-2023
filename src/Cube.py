57163f55d05ce41761f4d7793d122c4a098017d3
Kipp Puffalt <puffaltk@gmail.com>
2022-12-10 15:08:29 -0700

    Added the lt turning function

--- src/Cube.py
+++ src/Cube.py
@@ -1,16 +1,16 @@
 class Cube:
    def __init__(self):
-         # Array Layout: U1, U2, U3, U4, L1, L2, F1, F2, ....
-        self.cubies = [
-            ['W'],['W'],
-            ['W'],['W'],
-            ['R'],['R'],['B'],['B'],['O'],['O'],
-            ['R'],['R'],['B'],['B'],['O'],['O'],
-            ['Y'],['Y'],
-            ['Y'],['Y'],
-            ['G'],['G'],
-            ['G'],['G'],
-            ]
+      # Array Layout: U1, U2, U3, U4, L1, L2, F1, F2, ....
+      self.cubies = [
+         'W','W',
+         'W','W',
+         'R','R','B','B','O','O',
+         'R','R','B','B','O','O',         
+         'Y','Y',
+         'Y','Y',
+         'G','G',
+         'G','G',
+         ]
     
    # These are all the turning functions for a Rubik's cube (tudo)
     
@@ -51,8 +51,9 @@ class Cube:
    def lp(self):
       self.cubies[0], self.cubies[2], self.cubies[6], self.cubies[12], self.cubies[16], self.cubies[18], self.cubies[20], self.cubies[22], self.cubies[4], self.cubies[5], self.cubies[10], self.cubies[11] = self.cubies[6], self.cubies[12], self.cubies[16], self.cubies[18], self.cubies[20], self.cubies[22], self.cubies[0], self.cubies[2], self.cubies[5], self.cubies[11], self.cubies[4], self.cubies[10]
    # does a 180 turn to the left hand side of the cube
-    # def lt(self):
-
+   def lt(self):
+      self.cubies[0], self.cubies[2], self.cubies[6], self.cubies[12], self.cubies[16], self.cubies[18], self.cubies[20], self.cubies[22], self.cubies[4], self.cubies[5], self.cubies[10], self.cubies[11] = self.cubies[16], self.cubies[18], self.cubies[20], self.cubies[22], self.cubies[0], self.cubies[2], self.cubies[6], self.cubies[12], self.cubies[11], self.cubies[10], self.cubies[5], self.cubies[4]
+   
    # does a clockwise turn to the front of the cube
     # def f(self):
 
