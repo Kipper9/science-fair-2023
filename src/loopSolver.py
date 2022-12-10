a2c0194e5ee61089ebe0cd64e1547af826310ea9
Kipp Puffalt <puffaltk@gmail.com>
2022-12-10 16:13:26 -0700

    changed the file name

---
+++ src/loopSolver.py
@@ -0,0 +1,73 @@
+from Cube import Cube
+
+cube = Cube()
+
+while True:
+    turn = str(input('What is your turn: '))
+
+    if turn == 'f':
+        cube.f()
+    
+    elif turn == 'fp':
+        cube.fp()
+    
+    elif turn == 'ft':
+        cube.ft()
+    
+    elif turn == 'u':
+        cube.u()
+    
+    elif turn == 'up':
+        cube.up()
+   
+    elif turn == 'ut':
+        cube.ut()
+    
+    elif turn == 'd':
+        cube.d()
+    
+    elif turn == 'dp':
+        cube.d()
+    
+    elif turn == 'dt':
+        cube.dt()
+    
+    elif turn == 'r':
+        cube.r()
+    
+    elif turn == 'rp':
+        cube.rp()
+    
+    elif turn == 'rt':
+        cube.rt()
+    
+    elif turn == 'l':
+        cube.l()
+    
+    elif turn == 'lp':
+        cube.lp()
+    
+    elif turn == 'lt':
+        cube.lt()
+    
+    elif turn == 'b':
+        cube.b()
+    
+    elif turn == 'bp':
+        cube.bp()
+    
+    elif turn == 'bt':
+        cube.bt()
+    
+    elif turn == 'exit':
+        break
+    
+    else:
+        print('error: move', turn ,'is not indexed')
+
+    print ("The cube's state is:", end ='')
+    cube.render()
+
+    if cube.isSolved():
+        print('The cube is solved!')
+        break
