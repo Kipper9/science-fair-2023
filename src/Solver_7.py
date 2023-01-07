cd93698f3b8b336ddeb5585949cce90aabc0a89d
Kipp Puffalt <puffaltk@gmail.com>
2023-01-06 19:10:20 -0700

    added a load function and got the code working

--- src/Solver_7.py
+++ src/Solver_7.py
@@ -8,49 +8,37 @@ def load():
     global solvedCubes
     print('Importing')
     start_time = time.perf_counter()
-    with open('cubeDepth7c.pkl','rb') as f:
+    with open('cubeDepth7d.pkl','rb') as f:
         solvedCubes = pickle.load(f)
-    
     endTime = time.perf_counter()
     print('Finished import')
     print (f'It took: {endTime - start_time: 0.3f}')
 
 def parentSwap(cubeA, cubeB):
-    cube = cubeB
+    nextMove = None
     parent = cubeA
-    while cube.parent:
-        cube = cube.parent
-        newCube = Cube(cube.cubies, parent, cube.reverse())
+    cube = cubeB.parent
+    while cube:
+        newCube = Cube(cube.cubies, parent, nextMove or parent.move)
+        nextMove = cube.reverse()
         parent = newCube
-    
+        cube = cube.parent
     return parent
-        
 
 def solve(cube):
     visitedCubes = {}
     queue = collections.deque([(0, cube)])
-    maxDepth = 0
     
-    
-    start_time2 = time.perf_counter()
+    start_time = time.perf_counter()
     while queue:
         depth, currentCube = queue.popleft()
-        
-        # checks if we have found a  new depth
-        if depth > maxDepth:
-            maxDepth = depth
-            print('Searching depth: ',maxDepth)
 
         # checks if the cube is found in the presolved file
         if currentCube in solvedCubes:
-            endTime2 = time.perf_counter()
+            endTime = time.perf_counter()
             print('Solved, pickle') 
-            currentCube.render()
-            print('--')
-            solvedCubes[currentCube].render()
-            print('++++++++')
             parentSwap(currentCube, solvedCubes[currentCube]).render()
-            print (f'It took: {endTime2 - start_time2: 0.3f}')
+            print (f'It took: {endTime - start_time: 0.3f}')
             break
         
         # loops over each of the moves
