6236e5fafe5da22a5ce686f00680f449e0b789be
Kipp Puffalt <puffaltk@gmail.com>
2023-01-09 19:24:29 -0700

    a few small changes

--- src/test.py
+++ src/test.py
@@ -1,14 +1,15 @@
 import pickle
 from Cube import Cube
-with open('cubeDepth7b.pkl','rb') as f:
+with open('cubeDepth7d.pkl','rb') as f:
     cubes = pickle.load(f)
 
 cube = Cube(['R', 'R', 'W', 'W', 'W', 'O', 'G', 'G', 'R', 'Y', 'R', 'Y', 'B', 'B', 'W', 'O', 'O', 'O', 'Y', 'Y', 'G', 'G', 'B', 'B'])
 
 if cube in cubes:
-    solvedCube, depth = cubes[cube]
-    print('It worked. The depth was: ',depth, 'The cube is:')
+    solvedCube = cubes[cube]
+    print('It worked. The cube is: ')
     solvedCube.render()
+
 else:
     print(':(')
 
