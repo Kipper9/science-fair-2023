3a1a5fbeaaa2211e0d2beb749302f927d20ca3c8
Kipp Puffalt <puffaltk@gmail.com>
2023-01-10 19:31:03 -0700

    fixed the bug with the r functions

--- src/CurrentSolver/Cube.py
+++ src/CurrentSolver/Cube.py
@@ -19,114 +19,132 @@ class Cube:
    # does a clockwise twist to top of the cube
    def u(self):
       _cubies = self.cubies.copy()
-      _cubies[0], _cubies[1], _cubies[2], _cubies[3], _cubies[4], _cubies[5], _cubies[6], _cubies[7], _cubies[8], _cubies[9], _cubies[22], _cubies[23] = _cubies[2], _cubies[0], _cubies[3], _cubies[1], _cubies[6], _cubies[7], _cubies[8], _cubies[9], _cubies[23], _cubies[22], _cubies[5], _cubies[4]
+      _cubies[0], _cubies[1], _cubies[2], _cubies[3], _cubies[4], _cubies[5], _cubies[6], _cubies[7], _cubies[8], _cubies[9], _cubies[22], _cubies[23] = \
+      _cubies[2], _cubies[0], _cubies[3], _cubies[1], _cubies[6], _cubies[7], _cubies[8], _cubies[9], _cubies[23], _cubies[22], _cubies[5], _cubies[4]
 
       return Cube(_cubies, self,"U")
    # does a counterclockwise twist to top of the cube
    def up(self):
       _cubies = self.cubies.copy()
-      _cubies[0], _cubies[1], _cubies[2], _cubies[3], _cubies[4], _cubies[5], _cubies[6], _cubies[7], _cubies[8], _cubies[9], _cubies[22], _cubies[23] = _cubies[1], _cubies[3], _cubies[0], _cubies[2], _cubies[23], _cubies[22], _cubies[4], _cubies[5], _cubies[6], _cubies[7], _cubies[9], _cubies[8]
+      _cubies[0], _cubies[1], _cubies[2], _cubies[3], _cubies[4], _cubies[5], _cubies[6], _cubies[7], _cubies[8], _cubies[9], _cubies[22], _cubies[23] = \
+      _cubies[1], _cubies[3], _cubies[0], _cubies[2], _cubies[23], _cubies[22], _cubies[4], _cubies[5], _cubies[6], _cubies[7], _cubies[9], _cubies[8]
       
       return Cube(_cubies, self, "U'")
    # does a 180 turn to the top of the cube
    def ut(self):
       _cubies = self.cubies.copy()
-      _cubies[0], _cubies[1], _cubies[2], _cubies[3], _cubies[4], _cubies[5], _cubies[6], _cubies[7], _cubies[8], _cubies[9], _cubies[22], _cubies[23] = _cubies[3], _cubies[2], _cubies[1], _cubies[0], _cubies[8], _cubies[9], _cubies[23], _cubies[22], _cubies[4], _cubies[5], _cubies[7], _cubies[6]
+      _cubies[0], _cubies[1], _cubies[2], _cubies[3], _cubies[4], _cubies[5], _cubies[6], _cubies[7], _cubies[8], _cubies[9], _cubies[22], _cubies[23] = \
+      _cubies[3], _cubies[2], _cubies[1], _cubies[0], _cubies[8], _cubies[9], _cubies[23], _cubies[22], _cubies[4], _cubies[5], _cubies[7], _cubies[6]
       
       return Cube(_cubies, self,'U2')
 
    # does a clockwise turn to the bottom layer
    def d(self):
       _cubies = self.cubies.copy()
-      _cubies[10], _cubies[11], _cubies[12], _cubies[13], _cubies[14], _cubies[15], _cubies[16], _cubies[17], _cubies[18], _cubies[19], _cubies[21], _cubies[20] = _cubies[21], _cubies[20], _cubies[10], _cubies[11], _cubies[12], _cubies[13], _cubies[18], _cubies[16], _cubies[19], _cubies[17], _cubies[14], _cubies[15]
+      _cubies[10], _cubies[11], _cubies[12], _cubies[13], _cubies[14], _cubies[15], _cubies[16], _cubies[17], _cubies[18], _cubies[19], _cubies[21], _cubies[20] = \
+      _cubies[21], _cubies[20], _cubies[10], _cubies[11], _cubies[12], _cubies[13], _cubies[18], _cubies[16], _cubies[19], _cubies[17], _cubies[14], _cubies[15]
       
       return Cube(_cubies, self, 'D')
    # does a counterclockwise turn to the bottom layer
    def dp(self):
       _cubies = self.cubies.copy()
-      _cubies[10], _cubies[11], _cubies[12], _cubies[13], _cubies[14], _cubies[15], _cubies[16], _cubies[17], _cubies[18], _cubies[19], _cubies[21], _cubies[20] = _cubies[12], _cubies[13], _cubies[14], _cubies[15], _cubies[21], _cubies[20], _cubies[17], _cubies[19], _cubies[16], _cubies[18], _cubies[10], _cubies[11]
+      _cubies[10], _cubies[11], _cubies[12], _cubies[13], _cubies[14], _cubies[15], _cubies[16], _cubies[17], _cubies[18], _cubies[19], _cubies[21], _cubies[20] = \
+      _cubies[12], _cubies[13], _cubies[14], _cubies[15], _cubies[21], _cubies[20], _cubies[17], _cubies[19], _cubies[16], _cubies[18], _cubies[10], _cubies[11]
       
       return Cube(_cubies, self, "D'")
    # does a 180 turn  to the bottom layer
    def dt(self):
       _cubies = self.cubies.copy()
-      _cubies[10], _cubies[11], _cubies[12], _cubies[13], _cubies[14], _cubies[15], _cubies[16], _cubies[17], _cubies[18], _cubies[19], _cubies[21], _cubies[20] = _cubies[14], _cubies[15], _cubies[21], _cubies[20], _cubies[10], _cubies[11], _cubies[19], _cubies[18], _cubies[17], _cubies[16], _cubies[12], _cubies[13]
+      _cubies[10], _cubies[11], _cubies[12], _cubies[13], _cubies[14], _cubies[15], _cubies[16], _cubies[17], _cubies[18], _cubies[19], _cubies[21], _cubies[20] = \
+      _cubies[14], _cubies[15], _cubies[21], _cubies[20], _cubies[10], _cubies[11], _cubies[19], _cubies[18], _cubies[17], _cubies[16], _cubies[12], _cubies[13]
 
       return Cube(_cubies, self, "D2")
    
    # does a turn to the right hand side clockwise
    def r(self):
       _cubies = self.cubies.copy()
-      _cubies[3], _cubies[1], _cubies[7], _cubies[13], _cubies[17], _cubies[19], _cubies[21], _cubies[23], _cubies[8], _cubies[9], _cubies[14], _cubies[15] = _cubies[7], _cubies[13], _cubies[17], _cubies[19], _cubies[21], _cubies[23], _cubies[1], _cubies[3], _cubies[14], _cubies[8], _cubies[15], _cubies[9]
+      _cubies[1], _cubies[3], _cubies[7], _cubies[13], _cubies[17], _cubies[19], _cubies[21], _cubies[23], _cubies[8], _cubies[9], _cubies[14], _cubies[15] = \
+      _cubies[7], _cubies[13], _cubies[17], _cubies[19], _cubies[21], _cubies[23], _cubies[1], _cubies[3], _cubies[14], _cubies[8], _cubies[15], _cubies[9]
    
       return Cube(_cubies, self, 'R')
     # does a turn to the right hand side counterclockwise
    def rp(self):
       _cubies = self.cubies.copy()
-      _cubies[3], _cubies[1], _cubies[7], _cubies[13], _cubies[17], _cubies[19], _cubies[21], _cubies[23], _cubies[8], _cubies[9], _cubies[14], _cubies[15] = _cubies[21], _cubies[23], _cubies[1], _cubies[3], _cubies[7], _cubies[13], _cubies[17], _cubies[19], _cubies[9], _cubies[15], _cubies[8], _cubies[14]
+      _cubies[1], _cubies[3], _cubies[7], _cubies[13], _cubies[17], _cubies[19], _cubies[21], _cubies[23], _cubies[8], _cubies[9], _cubies[14], _cubies[15] = \
+      _cubies[21], _cubies[23], _cubies[1], _cubies[3], _cubies[7], _cubies[13], _cubies[17], _cubies[19], _cubies[9], _cubies[15], _cubies[8], _cubies[14]
    
       return Cube(_cubies, self, "R'")
     # does a 180 turn to the right han side of the cube
    def rt(self):
       _cubies = self.cubies.copy()
-      _cubies[3], _cubies[1], _cubies[7], _cubies[13], _cubies[17], _cubies[19], _cubies[21], _cubies[23], _cubies[8], _cubies[9], _cubies[14], _cubies[15] = _cubies[17], _cubies[19], _cubies[21], _cubies[23], _cubies[1], _cubies[3], _cubies[7], _cubies[13], _cubies[15], _cubies[14], _cubies[9], _cubies[8]
+      _cubies[1], _cubies[3], _cubies[7], _cubies[13], _cubies[17], _cubies[19], _cubies[21], _cubies[23], _cubies[8], _cubies[9], _cubies[14], _cubies[15] = \
+      _cubies[17], _cubies[19], _cubies[21], _cubies[23], _cubies[1], _cubies[3], _cubies[7], _cubies[13], _cubies[15], _cubies[14], _cubies[9], _cubies[8]
    
       return Cube(_cubies, self, "R2")
 
    # does a turn to the left hand side clockwise
    def l(self):
       _cubies = self.cubies.copy()
-      _cubies[0], _cubies[2], _cubies[6], _cubies[12], _cubies[16], _cubies[18], _cubies[20], _cubies[22], _cubies[4], _cubies[5], _cubies[10], _cubies[11] = _cubies[20], _cubies[22], _cubies[0], _cubies[2], _cubies[6], _cubies[12], _cubies[16], _cubies[18], _cubies[10], _cubies[4], _cubies[11], _cubies[5]
+      _cubies[0], _cubies[2], _cubies[6], _cubies[12], _cubies[16], _cubies[18], _cubies[20], _cubies[22], _cubies[4], _cubies[5], _cubies[10], _cubies[11] = \
+      _cubies[20], _cubies[22], _cubies[0], _cubies[2], _cubies[6], _cubies[12], _cubies[16], _cubies[18], _cubies[10], _cubies[4], _cubies[11], _cubies[5]
    
       return Cube(_cubies, self, 'L')
    # does a turn to the left hand side counterclockwise
    def lp(self):
       _cubies = self.cubies.copy()
-      _cubies[0], _cubies[2], _cubies[6], _cubies[12], _cubies[16], _cubies[18], _cubies[20], _cubies[22], _cubies[4], _cubies[5], _cubies[10], _cubies[11] = _cubies[6], _cubies[12], _cubies[16], _cubies[18], _cubies[20], _cubies[22], _cubies[0], _cubies[2], _cubies[5], _cubies[11], _cubies[4], _cubies[10]
+      _cubies[0], _cubies[2], _cubies[6], _cubies[12], _cubies[16], _cubies[18], _cubies[20], _cubies[22], _cubies[4], _cubies[5], _cubies[10], _cubies[11] = \
+      _cubies[6], _cubies[12], _cubies[16], _cubies[18], _cubies[20], _cubies[22], _cubies[0], _cubies[2], _cubies[5], _cubies[11], _cubies[4], _cubies[10]
    
       return Cube(_cubies, self, "L'")
    # does a 180 turn to the left hand side of the cube
    def lt(self):
       _cubies = self.cubies.copy()
-      _cubies[0], _cubies[2], _cubies[6], _cubies[12], _cubies[16], _cubies[18], _cubies[20], _cubies[22], _cubies[4], _cubies[5], _cubies[10], _cubies[11] = _cubies[16], _cubies[18], _cubies[20], _cubies[22], _cubies[0], _cubies[2], _cubies[6], _cubies[12], _cubies[11], _cubies[10], _cubies[5], _cubies[4]
+      _cubies[0], _cubies[2], _cubies[6], _cubies[12], _cubies[16], _cubies[18], _cubies[20], _cubies[22], _cubies[4], _cubies[5], _cubies[10], _cubies[11] = \
+      _cubies[16], _cubies[18], _cubies[20], _cubies[22], _cubies[0], _cubies[2], _cubies[6], _cubies[12], _cubies[11], _cubies[10], _cubies[5], _cubies[4]
    
       return Cube(_cubies, self, "L2")
    
    # does a clockwise turn to the front of the cube
    def f(self):
       _cubies = self.cubies.copy()
-      _cubies[2], _cubies[3], _cubies[8], _cubies[14], _cubies[17], _cubies[16], _cubies[11], _cubies[5], _cubies[6], _cubies[7], _cubies[12], _cubies[13] = _cubies[11], _cubies[5], _cubies[2], _cubies[3], _cubies[8], _cubies[14], _cubies[17], _cubies[16], _cubies[12], _cubies[6], _cubies[13], _cubies[7]
+      _cubies[2], _cubies[3], _cubies[8], _cubies[14], _cubies[17], _cubies[16], _cubies[11], _cubies[5], _cubies[6], _cubies[7], _cubies[12], _cubies[13] = \
+      _cubies[11], _cubies[5], _cubies[2], _cubies[3], _cubies[8], _cubies[14], _cubies[17], _cubies[16], _cubies[12], _cubies[6], _cubies[13], _cubies[7]
    
       return Cube(_cubies, self, 'F')
    # does a counterclockwise turn to the front of the cube
    def fp(self):
       _cubies = self.cubies.copy()
-      _cubies[2], _cubies[3], _cubies[8], _cubies[14], _cubies[17], _cubies[16], _cubies[11], _cubies[5], _cubies[6], _cubies[7], _cubies[12], _cubies[13] = _cubies[8], _cubies[14], _cubies[17], _cubies[16], _cubies[11], _cubies[5], _cubies[2], _cubies[3], _cubies[7], _cubies[13], _cubies[6], _cubies[12]
+      _cubies[2], _cubies[3], _cubies[8], _cubies[14], _cubies[17], _cubies[16], _cubies[11], _cubies[5], _cubies[6], _cubies[7], _cubies[12], _cubies[13] = \
+      _cubies[8], _cubies[14], _cubies[17], _cubies[16], _cubies[11], _cubies[5], _cubies[2], _cubies[3], _cubies[7], _cubies[13], _cubies[6], _cubies[12]
    
       return Cube(_cubies, self, "F'")
    # does a 180 turn to the front side of the cube
    def ft(self):
       _cubies = self.cubies.copy()
-      _cubies[2], _cubies[3], _cubies[8], _cubies[14], _cubies[17], _cubies[16], _cubies[11], _cubies[5], _cubies[6], _cubies[7], _cubies[12], _cubies[13] = _cubies[17], _cubies[16], _cubies[11], _cubies[5], _cubies[2], _cubies[3], _cubies[8], _cubies[14], _cubies[13], _cubies[12], _cubies[7], _cubies[6]
+      _cubies[2], _cubies[3], _cubies[8], _cubies[14], _cubies[17], _cubies[16], _cubies[11], _cubies[5], _cubies[6], _cubies[7], _cubies[12], _cubies[13] = \
+      _cubies[17], _cubies[16], _cubies[11], _cubies[5], _cubies[2], _cubies[3], _cubies[8], _cubies[14], _cubies[13], _cubies[12], _cubies[7], _cubies[6]
 
       return Cube(_cubies, self, 'F2')
 
    # does a clockwise turn to the backside of the cube
    def b(self):
       _cubies = self.cubies.copy()
-      _cubies[0], _cubies[1], _cubies[9], _cubies[15], _cubies[19], _cubies[18], _cubies[10], _cubies[4], _cubies[23], _cubies[22], _cubies[21], _cubies[20] = _cubies[9], _cubies[15], _cubies[19], _cubies[18], _cubies[10], _cubies[4], _cubies[0], _cubies[1], _cubies[21], _cubies[23], _cubies[20], _cubies[22]
+      _cubies[0], _cubies[1], _cubies[9], _cubies[15], _cubies[19], _cubies[18], _cubies[10], _cubies[4], _cubies[23], _cubies[22], _cubies[21], _cubies[20] = \
+      _cubies[9], _cubies[15], _cubies[19], _cubies[18], _cubies[10], _cubies[4], _cubies[0], _cubies[1], _cubies[21], _cubies[23], _cubies[20], _cubies[22]
    
       return Cube(_cubies, self, 'B')
    # does a counterclockwise turn to the backside of the cube
    def bp(self):
       _cubies = self.cubies.copy()
-      _cubies[0], _cubies[1], _cubies[9], _cubies[15], _cubies[19], _cubies[18], _cubies[10], _cubies[4], _cubies[23], _cubies[22], _cubies[21], _cubies[20] = _cubies[10], _cubies[4], _cubies[0], _cubies[1], _cubies[9], _cubies[15], _cubies[19], _cubies[18], _cubies[22], _cubies[20], _cubies[23], _cubies[21]
+      _cubies[0], _cubies[1], _cubies[9], _cubies[15], _cubies[19], _cubies[18], _cubies[10], _cubies[4], _cubies[23], _cubies[22], _cubies[21], _cubies[20] = \
+      _cubies[10], _cubies[4], _cubies[0], _cubies[1], _cubies[9], _cubies[15], _cubies[19], _cubies[18], _cubies[22], _cubies[20], _cubies[23], _cubies[21]
    
       return Cube(_cubies, self, "B'")
    # does a 180 turn to the back side of the cube
    def bt(self):
       _cubies = self.cubies.copy()
-      _cubies[0], _cubies[1], _cubies[9], _cubies[15], _cubies[19], _cubies[18], _cubies[10], _cubies[4], _cubies[23], _cubies[22], _cubies[21], _cubies[20] = _cubies[19], _cubies[18], _cubies[10], _cubies[4], _cubies[0], _cubies[1], _cubies[9], _cubies[15], _cubies[20], _cubies[21], _cubies[22], _cubies[23]
+      _cubies[0], _cubies[1], _cubies[9], _cubies[15], _cubies[19], _cubies[18], _cubies[10], _cubies[4], _cubies[23], _cubies[22], _cubies[21], _cubies[20] = \
+      _cubies[19], _cubies[18], _cubies[10], _cubies[4], _cubies[0], _cubies[1], _cubies[9], _cubies[15], _cubies[20], _cubies[21], _cubies[22], _cubies[23]
    
       return Cube(_cubies, self, 'B2')
    
