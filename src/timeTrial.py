f4f80d65331ded730f356aa991df3771dbe2b1d4
Kipp Puffalt <puffaltk@gmail.com>
2023-01-14 15:37:39 -0700

    added printing

--- src/timeTrial.py
+++ src/timeTrial.py
@@ -1,26 +1,25 @@
-import Solver_7 as solver
+import Solver_2 as solver
+import time
 from Cube import Cube
 
 cubes = [
     # depth 2
     Cube(['R', 'R', 'W', 'W', 'W', 'O', 'G', 'G', 'R', 'Y', 'R', 'Y', 'B', 'B', 'W', 'O', 'O', 'O', 'Y', 'Y', 'G', 'G', 'B', 'B']),
-    # Solution: Ut, Fp
     
     # depth 3
     Cube(['W', 'Y', 'O', 'G', 'B', 'Y', 'G', 'R', 'W', 'O', 'R', 'W', 'O', 'B', 'Y', 'G', 'B', 'R', 'Y', 'W', 'G', 'O', 'R', 'B']),
-    # Solution: F, Rt, Up
     
     # depth 4
     Cube(['W', 'Y', 'O', 'G', 'B', 'Y', 'G', 'R', 'W', 'O', 'Y', 'G', 'O', 'G', 'R', 'W', 'W', 'Y', 'R', 'B', 'B', 'O', 'R', 'B']),
-    # Solution: Ut, F, Ut, Rp
     
     # depth 5
     Cube(['W', 'Y', 'Y', 'W', 'B', 'R', 'G', 'O', 'G', 'O', 'Y', 'W', 'R', 'G', 'Y', 'W', 'G', 'O', 'R', 'B', 'B', 'O', 'R', 'B']),
-    # Solution: Ft, Ut, F, Ut, Rp
     
+    # depth 7
+    Cube(['R', 'B', 'G', 'W', 'B', 'O', 'Y', 'B', 'O', 'Y', 'R', 'W', 'R', 'W', 'G', 'R', 'G', 'O', 'Y', 'B', 'G', 'W', 'Y', 'O']),
+
     # depth 9
     Cube(['B', 'R', 'G', 'G', 'Y', 'R', 'W', 'O', 'Y', 'B', 'O', 'W', 'O', 'R', 'Y', 'O', 'B', 'G', 'W', 'B', 'G', 'Y', 'R', 'W']),
-    # Solution: Rp, Ut, Rp, Fp, Rt, Fp, U, Ft, Rt
 
     # depth 10
     Cube(['G', 'B', 'B', 'R', 'Y', 'Y', 'O', 'Y', 'B', 'O', 'W', 'W', 'B', 'R', 'Y', 'W', 'R', 'G', 'R', 'O', 'G', 'G', 'O', 'W']),
@@ -32,5 +31,11 @@ solver.load()
 print('')
 
 for x, cube in enumerate(cubes):
-    solver.solve(cube)
-    print("")
+    print('Solving:    ', end='')
+    cube.render()
+    start_time = time.perf_counter()
+    solution = solver.solve(cube)
+    endTime = time.perf_counter()
+    print('Solution: ', solution.moves()," (", solution.moves().count(" ") - 1, " moves)", sep = '')
+    print (f'Time:      {endTime - start_time: 0.9f}s')
+    print('')
