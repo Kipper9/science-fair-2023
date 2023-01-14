bca69a23d940d39f402e2bc110567291f541bb10
Kipp Puffalt <puffaltk@gmail.com>
2023-01-14 15:37:02 -0700

    removed printing

--- src/Solver_1.py
+++ src/Solver_1.py
@@ -1,19 +1,13 @@
-import time
+def load():
+    pass
 def solve(cube):
-    start_time = time.perf_counter()
     visitedCubes = set()
     queue = [cube]
-
     while queue:
         currentCube = queue.pop(0)
-
         # checks if the cube is solved
         if currentCube.isSolved():
-            endTime = time.perf_counter()
-            print('Solved!') 
-            currentCube.render()
-            print (f'It took: {endTime - start_time: 0.4f}')
-            break
+            return currentCube
 
         # loop over each of the moves
         moves = [currentCube.u(), currentCube.up(), currentCube.ut(), currentCube.d(), currentCube.dp(), currentCube.dt(), currentCube.r(), currentCube.rp(), currentCube.rt(), currentCube.l(), currentCube.lp(), currentCube.lt(), currentCube.f(), currentCube.fp(), currentCube.ft(), currentCube.b(), currentCube.bp(), currentCube.bt()]
--- src/Solver_2.py
+++ src/Solver_2.py
@@ -1,19 +1,13 @@
-import time
+def load():
+    pass
 def solve(cube):
-    start_time = time.perf_counter()
     visitedCubes = set()
     queue = [cube]
-
     while queue:
         currentCube = queue.pop(0)
-
         # checks if the cube is solved
         if currentCube.isSolved():
-            endTime = time.perf_counter()
-            print('Solved!') 
-            currentCube.render()
-            print (f'It took: {endTime - start_time: 0.3f}')
-            break
+            return currentCube
 
         # loop over each of the moves
         moves = [currentCube.u(), currentCube.up(), currentCube.ut(), currentCube.r(), currentCube.rp(), currentCube.rt(), currentCube.f(), currentCube.fp(), currentCube.ft()]
