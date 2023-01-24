484e9ac904ad02df1c84a0881364fb4ac33628f7
Kipp Puffalt <puffaltk@gmail.com>
2023-01-23 20:37:03 -0700

    minor change

--- src/Cube.py
+++ src/Cube.py
@@ -198,7 +198,7 @@ class Cube:
 
    def reset(self):
       self.parent = None
-      self.moves = ''
+      self.move = None
 
    def __eq__(self, other):
       return self.cubies == other.cubies
