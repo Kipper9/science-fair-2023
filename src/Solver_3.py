c23813c88819e771d04056a1d54c00412d1f949b
Kipp Puffalt <puffaltk@gmail.com>
2023-02-05 13:06:16 -0700

    Renamed

--- src/Solver_3.py
+++ src/Solver_3.py
@@ -1,26 +1,45 @@
 import time
-def solve(cube):
+import collections
+import pickle
+from Cube import Cube
+solvedCubes = {}
+
+def load():
+    global solvedCubes
+    print('Importing')
     start_time = time.perf_counter()
+    with open('cubeDepth.pkl','rb') as f:
+        solvedCubes = pickle.load(f)
+    endTime = time.perf_counter()
+    print('Finished import')
+    print (f'Time: {endTime - start_time: 0.3f}s')
+
+def parentSwap(cubeA, cubeB):
+    nextMove = cubeB.move
+    parent = cubeA
+    cube = cubeB.parent
+    while cube:
+        newCube = Cube(cube.cubies, parent, nextMove or parent.move)
+        nextMove = cube.reverse()
+        parent = newCube
+        cube = cube.parent
+    return parent
+
+def solve(cube):
     visitedCubes = {}
-    queue = [(0, cube)]
-
+    queue = collections.deque([(0, cube)])
     while queue:
-        depth, currentCube = queue.pop(0)
-
-        # checks if the cube is solved
-        if currentCube.isSolved():
-            endTime = time.perf_counter()
-            print('Solved!') 
-            currentCube.render()
-            print (f'It took: {endTime - start_time: 0.3f}')
-            break
-
-        # loop over each of the moves
-        moves = [currentCube.u(), currentCube.up(), currentCube.ut(), currentCube.r(), currentCube.rp(), currentCube.rt(), currentCube.f(), currentCube.fp(), currentCube.ft()]
+        depth, currentCube = queue.popleft()
+        # checks if the cube is found in the presolved file
+        if currentCube in solvedCubes:
+            return parentSwap(currentCube, solvedCubes[currentCube])
+        # loops over each of the moves
+        moves = [currentCube.d(), currentCube.dp(), currentCube.dt(), 
+        currentCube.l(), currentCube.lp(), currentCube.lt(), 
+        currentCube.b(), currentCube.bt(),currentCube.bp()]
         for newcube in moves:
-            if not newcube in visitedCubes.get(depth, set()) or not newcube in visitedCubes.get(depth - 1, set()) or not newcube in visitedCubes.get(depth - 2, set()):
+            if not newcube in visitedCubes:
                 queue.append((depth + 1, newcube))
-            
             if not depth in visitedCubes:
                 visitedCubes[depth] = set()
-            visitedCubes[depth].add(currentCube)
+        visitedCubes[depth].add(currentCube)
---
+++ src/Test_1.py
@@ -0,0 +1,26 @@
+import time
+def solve(cube):
+    start_time = time.perf_counter()
+    visitedCubes = {}
+    queue = [(0, cube)]
+
+    while queue:
+        depth, currentCube = queue.pop(0)
+
+        # checks if the cube is solved
+        if currentCube.isSolved():
+            endTime = time.perf_counter()
+            print('Solved!') 
+            currentCube.render()
+            print (f'It took: {endTime - start_time: 0.3f}')
+            break
+
+        # loop over each of the moves
+        moves = [currentCube.u(), currentCube.up(), currentCube.ut(), currentCube.r(), currentCube.rp(), currentCube.rt(), currentCube.f(), currentCube.fp(), currentCube.ft()]
+        for newcube in moves:
+            if not newcube in visitedCubes.get(depth, set()) or not newcube in visitedCubes.get(depth - 1, set()) or not newcube in visitedCubes.get(depth - 2, set()):
+                queue.append((depth + 1, newcube))
+            
+            if not depth in visitedCubes:
+                visitedCubes[depth] = set()
+            visitedCubes[depth].add(currentCube)
