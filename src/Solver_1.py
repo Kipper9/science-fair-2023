136cb69f694abc64169a3e3ee379a5eb9f1ee2ac
Kipp Puffalt <puffaltk@gmail.com>
2023-11-03 14:52:38 -0600

    formating

--- src/Solver_1.py
+++ src/Solver_1.py
@@ -10,7 +10,12 @@ def solve(cube):
             return currentCube
 
         # loop over each of the moves
-        moves = [currentCube.u(), currentCube.up(), currentCube.ut(), currentCube.d(), currentCube.dp(), currentCube.dt(), currentCube.r(), currentCube.rp(), currentCube.rt(), currentCube.l(), currentCube.lp(), currentCube.lt(), currentCube.f(), currentCube.fp(), currentCube.ft(), currentCube.b(), currentCube.bp(), currentCube.bt()]
+        moves = [currentCube.u(), currentCube.up(), currentCube.ut(),
+        currentCube.d(), currentCube.dp(), currentCube.dt(), 
+        currentCube.r(), currentCube.rp(), currentCube.rt(), 
+        currentCube.l(), currentCube.lp(), currentCube.lt(), 
+        currentCube.f(), currentCube.fp(), currentCube.ft(), 
+        currentCube.b(), currentCube.bp(), currentCube.bt()]
         for newcube in moves:
             if not newcube in visitedCubes:
                 queue.append(newcube)
